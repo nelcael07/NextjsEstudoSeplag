619b83386f4e7b2fdbe1b2d0a4c6c7bb90676c8d
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-07-01 16:47:04 -0400

    [Implementação] - Roters

---
+++ nextjs/pages/Testando.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testando = () => {
+  return (
+    <>
+      <h1>Pagina de testando!!!</h1>
+    </>
+  )
+}
+
+export default Testando
--- nextjs/pages/users.js
+++ nextjs/pages/users.js
@@ -1,47 +1,46 @@
-import { useEffect, useState } from "react";
+import {useState,useEffect} from 'react'
 import axios from 'axios'
+import Link from 'next/link'
 
-const users = ({usuario}) => {
+const users = () => {
+  // MANEIRA DE FAZER UMA REQUISIÇÃO DO LADO DO CLIENTE
 
-    // MANEIRA DE FAZER UMA REQUISIÇÃO DO LADO DO CLIENTE
+  const [usuario, setUsuario] = useState([]);
 
-    // const [usuario, setUsuario] = useState([]);
+  useEffect(() => {
+    fetchUsers()
+  }, []);
 
-    // useEffect(() => {
-    //     fetchUsers()
-    // }, []);
-
-
-    // async function fetchUsers() {
-    //     // utilização do axios para pegar os dados da api
-    //     const response =  await axios.get('https://jsonplaceholder.typicode.com/users');
-    //     const data = await response.data;
-    //     setUsuario(data)
-    // }
-
-    return (
-        <div>
-            {usuario.map((item,key)=>(
-                <div
-                    key={key}
-                >
-                    {item.name}
-            </div>
-            ))}
-        </div>
-    );
+  async function fetchUsers() {
+    // utilização do axios para pegar os dados da api
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    const data = await response.data
+    setUsuario(data)
+  }
+  return (
+    <div>
+      <button><Link href='/Testando'>Clique aqui!!!!!!!</Link></button>
+      <div>
+        {usuario.map((item, key) => (
+          <div
+            key={key}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 // MANEIRA DE FAZER UMA REQUISIÇÃO DO LADO SERVIDOR DA APLICAÇÃO
-export async function getServerSideProps(context) {
-    const response =  await axios.get('https://jsonplaceholder.typicode.com/users');
-    const data = await response.data;
-    
-    return{
-        props:{usuario: data},
-    }
-}
 
+// export async function getServerSideProps (context) {
+//   const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+//   const data = await response.data
+//   return {
+//     props: { usuario: data }
+//   }
+// }
 
-export default users;
-
+export default users
